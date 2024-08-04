63eb13908c361801331e7c2e1b365f34f19c405b
oiluj <juliogabriel.pe@gmail.com>
2024-08-04 00:39:26 -0300

    1 parte concluída: Script de ETL bronze para silver

--- script/etl.py
+++ script/etl.py
@@ -5,79 +5,104 @@ import pyarrow.parquet as pq
 from google.cloud import bigquery
 
 
-#Criar diretório bronze
-if os.path.exists('src'):
-    pass
-else:
-    os.mkdir('src')
+def criar_diretorio(diretorio):
+    if not os.path.exists(diretorio):   # Criar diretório dos dados brutos
+        os.mkdir(diretorio)
 
 
-#Importar dados do site do TLC NYC
-url1 = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-05.parquet'
-url2 = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
-
-response = requests.get(url1)
-with open('src/viagens.parquet', 'wb') as f:
-    f.write(response.content)
-    print('viagens.parquet extraído')
-
-response = requests.get(url2)
-with open('src/locais.csv', 'wb') as f:
-    f.write(response.content)
-    print('locais.csv extraído')
+def extract(url, destino):
+    '''Função que extrai os dados brutos da fonte'''
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        with open(destino, 'wb') as f:
+            f.write(response.content)
+        print(f'{destino} extraído')
+    except requests.exceptions.RequestException as exception:
+        print(f'{exception}')
 
 
-#Transformação
-data = pq.read_table('src/viagens.parquet')
-df = data.to_pandas()
+def transform(df):
+    '''Função que limpa os dados da tabela viagens para de acordo com os requisitos de negócio'''
+    df = df.loc[df['VendorID'] != 6,:]
+    df = df.loc[df['fare_amount'] > 0, :] #retirar linhas com dados incongruentes
+    df = df.loc[df['extra'] > 0, :]
+    df = df.drop(columns='total_amount')
 
-df = df.loc[df['VendorID'] != 6,:]
-df = df.loc[df['fare_amount'] > 0, :]        #retirar linhas com dados incongruentes
-df = df.loc[df['extra'] > 0, :]
-df = df.drop(columns='total_amount')
+    df['passenger_count'] = df['passenger_count'].astype('Int64')
+    df['RatecodeID'] = df['RatecodeID'].astype('Int64')
+    df['payment_type'] = df['payment_type'].astype('Int64')
 
-df['passenger_count'] = df['passenger_count'].astype('Int64')
-df['RatecodeID'] = df['RatecodeID'].astype('Int64')
-df['payment_type'] = df['payment_type'].astype('Int64')
+    df.loc[~df['RatecodeID'].isin([1, 2, 3, 4, 5, 6]),'RatecodeID'] = None  #converter ids não existentes em Nan
+    df.loc[~df['payment_type'].isin([1, 2, 3, 4, 5, 6]),'payment_type'] = None
 
-df.loc[~df['RatecodeID'].isin([1, 2, 3, 4, 5, 6]),'RatecodeID'] = None                  #converter ids não existentes em Nan
-df.loc[~df['payment_type'].isin([1, 2, 3, 4, 5, 6]),'payment_type'] = None
-
-df = df.reset_index(drop=True)
-df['ID'] = df.index         #criar id indice das viagens
-print('Tranformação efetuada')
+    df = df.reset_index(drop=True)
+    df['ID'] = df.index   #criar id indice das viagens
+    print('Tranformação efetuada')
+    
+    return df
 
 
-#Carregamento
-client = bigquery.Client()
+def load(fonte, tabela, schema, formato, **kwargs):
+    '''Função que carrega os dados limpos para o BigQuery'''
+    client = bigquery.Client()
+    dataset = client.create_dataset('silver', exists_ok=True) #Cria dataset caso não exista
 
-dataset = client.create_dataset('silver', exists_ok=True)       #Cria dataset silver
+    schema = client.schema_from_json(schema)  #Associa o json do schema
 
-# Tabela Viagens
-#    Definir o esquema da tabela
-schema = client.schema_from_json('schemas/schema_viagens.json')
+    job_config = bigquery.LoadJobConfig( #Configuração do Job
+        source_format=formato, 
+        schema=schema, 
+        **kwargs
+        )
+    
+    table = dataset.table(tabela) #Cria tabela
 
-#   Criação da tabela silver_viagens
-tabela = dataset.table('viagens')
-
-#   Configuração do Job
-job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.PARQUET, schema=schema)
-
-load_job = client.load_table_from_dataframe(df, tabela, job_config=job_config)
-load_job.result()
-print(f'Tabela {tabela} carregada para o BigQuery')
-
-# Tabela Locais
-with open('src/locais.csv', 'rb') as locais:
-    #   Definir o esquema da tabela
-    schema = client.schema_from_json('schemas/schema_locais.json')
-
-    #   Criação da tabela silver_locais
-    tabela = dataset.table('locais')
-
-    #   Configuração do Job
-    job_config = bigquery.LoadJobConfig(source_format=bigquery.SourceFormat.CSV, schema=schema, skip_leading_rows=1)
-
-    load_job = client.load_table_from_file(locais, tabela, job_config=job_config)
-    load_job.result()
+    if formato == bigquery.SourceFormat.PARQUET: #Diferencia entre a fonte Parquet e a fonte CSV
+        load_job = client.load_table_from_dataframe(fonte, table, job_config=job_config)
+    else:
+        load_job = client.load_table_from_file(fonte, table, job_config=job_config)
+    
+    load_job.result() #Executa o Job
     print(f'Tabela {tabela} carregada para o BigQuery')
+
+
+def main():
+    criar_diretorio('data')
+
+    #Extract
+    extract(
+        'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-05.parquet',
+        'data/viagens.parquet'
+        ) #Viagens
+
+    
+    extract(
+        'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv',
+        'data/locais.csv'
+        ) #Locais
+    
+    #Transform
+    data = pq.read_table('data/viagens.parquet')
+    df_viagens = transform(data.to_pandas())
+
+    #Load
+    load(
+        fonte=df_viagens,
+        tabela='viagens',
+        schema='schemas/schema_viagens.json',
+        formato=bigquery.SourceFormat.PARQUET
+        ) #Viagens
+    
+    with open('data/locais.csv', 'rb') as df_locais:
+        load(
+            fonte=df_locais,
+            tabela='locais',
+            schema='schemas/schema_locais.json',
+            formato=bigquery.SourceFormat.CSV,
+            skip_leading_rows=1
+        ) #Locais
+    
+
+if __name__ == '__main__':
+    main()
